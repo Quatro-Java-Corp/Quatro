97d14a3ee383b527fca01ed67ebe71fbf3a73a56
Spajro <rafal.spyra@onet.pl>
2023-04-03 20:41:17 +0200

    add negative values validation

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -10,26 +10,44 @@ public class Rectangle implements Figure {
     }
 
     public static Rectangle withShortSideAndLongSide(double shortSideLength, double longSideLength) {
+        if (shortSideLength < 0 || longSideLength < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         return new Rectangle(shortSideLength, longSideLength);
     }
 
     public static Rectangle withShortSideAndDiagonal(double shortSideLength, double diagonalLength) {
+        if (shortSideLength < 0 || diagonalLength < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         return new Rectangle(shortSideLength, sqrt(pow(diagonalLength, 2) - pow(shortSideLength, 2)));
     }
 
     public static Rectangle withShortSideAndSurfaceArea(double shortSideLength, double surfaceArea) {
+        if (shortSideLength < 0 || surfaceArea < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         return new Rectangle(shortSideLength, surfaceArea / shortSideLength);
     }
 
     public static Rectangle withLongSideAndDiagonal(double longSideLength, double diagonalLength) {
+        if (longSideLength < 0 || diagonalLength < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         return new Rectangle(sqrt(pow(diagonalLength, 2) - pow(longSideLength, 2)), longSideLength);
     }
 
     public static Rectangle withLongSideAndSurfaceArea(double longSideLength, double surfaceArea) {
+        if (longSideLength < 0 || surfaceArea < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         return new Rectangle(surfaceArea / longSideLength, longSideLength);
     }
 
     public static Rectangle withDiagonalAndSurfaceArea(double diagonalLength, double surfaceArea) {
+        if (diagonalLength < 0 || surfaceArea < 0) {
+            throw new IllegalArgumentException("Values must be non-negative");
+        }
         double val = sqrt(sqrt(pow(diagonalLength, 4) - 4 * pow(surfaceArea, 2)) + pow(diagonalLength, 2));
         return new Rectangle(abs(val / sqrt(2)), abs(sqrt(2) * surfaceArea / val)
         );
